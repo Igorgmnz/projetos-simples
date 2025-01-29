78914fc374be3d256d9fbe0b0417af3d12ef1d63
igorgmnz <igorgmnzmonte@gmail.com>
2025-01-29 13:34:38 -0300

    projetos antigos

---
+++ python de cria/main.py
@@ -0,0 +1,7 @@
+rola = int (input("insira o tamanho da tua peça: "))
+if rola < 10:
+     print("Mini picakkkkkkkkk")
+elif rola < 16:
+         print("Bela peça irmao")
+else:
+     print("Caralho que rolaokkkkkkkk")

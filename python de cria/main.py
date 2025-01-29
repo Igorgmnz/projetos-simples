24429cf49b838ab4e82c6f2bf93b0bdf9f5d7577
Igorgmnz <igorgmnzmonte@gmail.com>
2025-01-29 13:41:08 -0300

    Delete python de cria/main.py

--- python de cria/main.py
+++
@@ -1,7 +0,0 @@
-rola = int (input("insira o tamanho da tua peça: "))
-if rola < 10:
-     print("Mini picakkkkkkkkk")
-elif rola < 16:
-         print("Bela peça irmao")
-else:
-     print("Caralho que rolaokkkkkkkk")
